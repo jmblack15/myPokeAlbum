61b201d61476e847f1131425df2a6271e0b69d45
Jose <jm157653@gmail.com>
2024-05-20 19:49:29 -0500

    Adding routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AlbumComponent } from './pages/album/album.component';
+import { ObtainSheetsComponent } from './pages/obtain-sheets/obtain-sheets.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'album',component: AlbumComponent},
+  { path: 'sheets',component: ObtainSheetsComponent},
+  { path: '', redirectTo: '/album', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
---
+++ src/app/pages/obtain-sheets/obtain-sheets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-obtain-sheets',
+  standalone: true,
+  imports: [],
+  templateUrl: './obtain-sheets.component.html',
+  styleUrl: './obtain-sheets.component.css'
+})
+export class ObtainSheetsComponent {
+
+}
